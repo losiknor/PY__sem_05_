baef4f2c86cdc4106ea30f4272f423826cc13ee6
Larina <Losiknor@gmail.com>
2022-05-30 10:48:15 +0700

    Домашка - четвёртая задача

---
+++ domashka pred_4.py
@@ -0,0 +1,32 @@
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 1 до 100,
+# можно создать свой генератор случайных чисел или использовать готовый)
+# многочлена и записать в файл многочлен степени k.
+#
+# Пример:
+#
+# k=2 => 2*x² + 4*x + 5 = 0
+import random
+
+# from useFulFutires.IsNumber import isNumber
+
+k = 5# isNumber()
+count = k
+randomKoef = [random.randint(0, 100) for i in range(k+1)]
+
+resultT = ""
+for i in range(k):
+    if i == k-1:
+        resultT += str(randomKoef[i]) + '*x + '
+        resultT += str(randomKoef[i+1])
+        break
+    else:
+        resultT += str(randomKoef[i]) + f'*x**{count} + '
+    count -= 1
+
+resultT += ' = 0'
+print(resultT)
+path = 'randomCoefPolynom.txt'
+
+with open(path, 'w') as d:
+    d.write(resultT)
