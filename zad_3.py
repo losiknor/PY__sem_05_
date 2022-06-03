33d692fac365e12227b589ad93e2b23fbbe82e67
Larina <Losiknor@gmail.com>
2022-06-04 00:29:19 +0700

    Задача 3 - решение через включения

--- zad_3.py
+++ zad_3.py
@@ -1,1 +1,15 @@
 # Напишите программу, удаляющую из текста все слова, содержащие "абв".
+my_str = 'Напишите програбвмму, удабвляющую из текстабв все слова, содержащие абв'
+my_str = my_str.split(' ')
+print(my_str)
+res = ''
+for word in my_str:
+    if word.find('абв') == -1:
+        res += word + ' '
+
+print(res)
+
+#
+# res = [word for word in my_str if (word.find('абв') == -1)]
+# print(res)
+
